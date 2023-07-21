0dcfdafc0c6f856d362c022c90cf2e1c05ed41a9
Christian Klint Labadia <cklabadia@hotmail.com>
2023-07-21 15:10:53 +0800

    Remake whole notes to fix notebook display bug.

--- mysql_01.sql
+++ mysql_01.sql
@@ -1,12 +1,21 @@
 /*markdown
-# MySQL 1 - The Basics
-This is my personal notes on MySQL. The concepts can be found on other SQL variants. Formatting and styling is loose and may evolve throughout my note taking. I might adopt this [SQL style guide](https://github.com/mattm/sql-style-guide) later.
+# MySQL 01 - Overview
+
+This overview will show quick examples on some MySQL commands and features. Formatting and styling may be inconsistent at first and may evolve overtime as I adopt this [SQL style guide](https://github.com/mattm/sql-style-guide).
 */
 
 /*markdown
-#TODO 
+## Basic Syntax
 
-- Fix formatting (CRLF LF bug on SQL notebook extension)
+#TODO
+ - This section
+*/
+
+/*markdown
+Topics
+- terms (clauses, keywords, commands, etc)
+- semantic
+- syntax
 */
 
 /*markdown
@@ -17,31 +26,51 @@ This is my personal notes on MySQL. The concepts can be found on other SQL varia
 ### Database Basics
 */
 
--- Display all databases in server
+-- This cell is not meant to be run. This is for syntax
+
+-- Display all databases in server.
 SHOW DATABASES;
 
 -- Create an empty database.
 CREATE DATABASE <db_name>;
 
--- Delete database. Can't be undo, be careful.
+-- Delete database. This can't be undone (normally), be careful.
 DROP DATABASE <db_name>;
 
--- Selects DB and makes it the current active DB
-USE <db_name>
+-- Select database and make it the current active database.
+-- Most commands and SQL statements are this step is done.
+USE <db_name>;
 
--- Check active DB
+-- See what database is currently active.
 SELECT DATABASE();
 
--- Example
+/*markdown
+### Database Basics - Example
+*/
+
+-- Displays all databases in server.
 SHOW DATABASES;
+
+-- Create two databases.
 CREATE DATABASE pet_shelter;
-CREATE DATABASE incorrect_database;
+CREATE DATABASE bad_database;
 
-DROP DATABASE incorrect_database;
+-- Confirm if databases are now on the list.
 SHOW DATABASES;
 
-USE pet_shelter
-SELECT DATABASE()
+-- Delete bad_database.
+DROP DATABASE bad_database;
+
+-- Confirm that bad_database has been deleted.
+SHOW DATABASES;
+
+-- Select database.
+USE pet_shelter;
+
+-- Confirm that database is now the active one.
+SELECT DATABASE();
+
+-- We can now run SQL statements on our pet_shelter database!
 
 /*markdown
 ## Tables
@@ -51,244 +80,259 @@ SELECT DATABASE()
 ### Creating Tables
 */
 
--- Create table inside DB
+-- Create table inside database.
 CREATE TABLE <table_name> (
-    <field_1> <data_type_1>,
-    <field_2> <data_type_2>,
+    <field_1> <field1_data_type>,
+    <field_2> <field2_data_type>
 );
--- Fields are column/headers.
+-- Field is just an SQL term for columns.
 
+/*markdown
+### Creating Tables - Example
+*/
 
--- Show tables in currently active database
-SHOW TABLES;
-
--- Show table information
-SHOW COLUMNS FROM <table_name>;
-
--- Alternate way of showing table information
--- using DESCRIBE or DESC
-DESC <table_name>;
-
--- Example
+-- Create a table named cats inside our pet_shelter database.
+-- The cats table will have 2 fields, name and age, with the data type
+-- VARCHAR and INT, respectively.
 CREATE TABLE cats (
     name VARCHAR(50),
     age INT
 );
 
-CREATE TABLE dogs (
-    name VARCHAR(50),
-    breed VARCHAR(50),
-    age INT
-);
-
+-- Create a table for the sole purpose of being deleted later.
 CREATE TABLE wrong_table (
-    name VARCHAR(25),
-    number int
+    some_name VARCHAR(25),
+    some_number int
 );
 
--- Show current tables in database
+/*markdown
+### Other Table Commands
+*/
+
+-- Show tables in currently active database.
 SHOW TABLES;
-SHOW COLUMNS FROM dogs;
--- Using DESCRIBE
-DESC dogs;
+
+-- Show table information.
+SHOW COLUMNS FROM <table_name>;
+-- Alternatively, we can use the DESCRIBE or DESC keyword.
+DESCRIBE <table_name>;
+DESC <table_name>;
+
+/*markdown
+### Other Table Commands - Example
+*/
+
+SHOW TABLES;
+
+-- Show relevant information about the cats table.
+DESC cats;
+
+/*markdown
+### Inserting (Adding) Data to Table
+
+This is discussed more in the next chapter and is only discussed here for the purpose of the succeeding topics (default and null values).
+*/
+
+INSERT INTO <table_name> (
+    <field_1>,
+    <field_2>
+)
+VALUES (
+    <field1_value>,
+    <field2_value>
+);
+
+/*markdown
+### Inserting (Adding) Data to Table - Example
+*/
+
+INSERT INTO cats (
+    name,
+    age
+)
+VALUES (
+    'Tabby',
+    3
+);
 
 /*markdown
 ### Dropping (Deleting) Tables
 */
 
--- Delete a table. Again, be careful of using DROP as there is no confirmation when using it.
+-- Delete a table. Be careful as there is no confirmation and 
+-- this can't be undone in normal circumstances.
 DROP TABLE <table_name>;
 
--- Deleting from a specific database. This works even if the database is not currently selected/active.
--- Note that ( ` ) is backtick, not apostrophe ( ' ).
+-- Deleting from a specific database. This works even if the database is
+-- not currently active.
+-- Note that backticks ( ` ) are used, not apostrophes ( ' ).
 DROP TABLE `<db_name>`.`<table_name>`;
 
--- Example
-DROP TABLE wrong_table;
-
 /*markdown
-### Inserting (Adding) Data Into Table
+### Dropping (Deleting) Tables - Example
 */
 
--- Insert row into database table.
-INSERT INTO <table> (<field_1>, <field_2>)
-VALUES (<field_1_value>, <field_2_value>);
+DROP TABLE wrong_table;
 
--- Inserting values on ALL fields, the fields can be empty:
-INSERT INTO <table>
-VALUES (<field_1_value>, <field_2_value>);
-
--- Multiple insert
-INSERT INTO <table> (<field_a>, <field_b>)
-VALUES 
-    (<row_1a_val>, <row_1b_value>),
-    (<row_2a_val>, <row_2b_value>),
-    (<row_3a_val>, <row_3b_value>);
-
--- Show all (indicated *) columns of entries in a table.
--- SELECT is further discussed later.
-SELECT * FROM <table>;
-
--- Example
--- Insert a row in the cats table for a cat named Purrson, age 3.
-INSERT INTO cats (name, age)
-VALUES ('Purrson', 3);
-
--- View cats table to confirm added entry.
-SELECT * FROM cats;
+-- Confirm wrong_table is no longer on the list.
+SHOW TABLES;
 
 /*markdown
 ## Null and Default Values
+
+NULL, NaN, or NA values are empty or unknown values. This occurs for a variety of reasons, most commonly by not entering a value for a field, or errors (both human or system).
 */
 
 /*markdown
-### NOT NULL
+### NOT NULL Keyword
+
+The NOT NULL keyword will forbid NULL values in the field. This will reject adding in records containing a NULL value in the constrained field.
+
+
 */
 
--- NULL Value
--- NULL values are empty values or unknown values. We can use
--- the keywords NOT NULL to forbid NULL values in our table.
+-- Create a table with 2 fields field_1 and field_2.
+-- field_1 will not allow NULL values, but field_2 will.
 CREATE TABLE <table_name> (
-    <field_1> <data_type_1> NOT NULL,
-    <field_2> <data_type_2>,
+    <field_1> <field1_data_type> NOT NULL,
+    <field_2> <field2_data_type>
 );
--- The following table will have 2 fields: field_1 and field_2.
--- field_1 will not allow NULL values, but field 2 will.
 
--- Example
--- Create a cats table where the breed field, 
--- and only the breed field, can be NULL.
+/*markdown
+### NOT NULL Keyword - Example
+*/
+
+-- Create a cats2 table where only the breed field can be NULL.
 CREATE TABLE cats2 (
     name VARCHAR(100) NOT NULL,
-    breed VARCHAR(100),
-    age INT NOT NULL
+    breed VARCHAR(100)
 );
 
 /*markdown
-### Default Value for Fields
+### Field Default Value
+
+We can create a default value for a field if no value is provided for that field.
 */
 
--- We can create a field in a table where a default value
--- will be provided if no value is provided in that field.
 CREATE TABLE <table_name> (
-    <field> <field_type> DEFAULT <field_default>
+    <field> <field_type> DEFAULT <default_value>
 );
 
--- Example
+-- The DEFAULT keyword can also be used to insert the default value, when possible.
+INSERT INTO <table_name> (
+    <field_1>,
+    <field_2>
+)
+VALUES (
+    DEFAULT,
+    <field2_value>
+);
+
+/*markdown
+### Field Default Value - Example
+*/
+
+-- Create a cats3 table where the age is defaulted to 0 if no age is provided.
 CREATE TABLE cats3 (
-    name VARCHAR(100) DEFAULT 'Tabby',
-    age INT
+    name VARCHAR(100),
+    age INT DEFAULT 0
 );
 
--- Note that no value is provided for the "name" field.
--- This will add an entry of a cat named Tabby (default) aged 3.
-INSERT INTO cats3 (age)
-VALUES (3);
-
-SELECT * FROM cats3;
-
-/*markdown
-### Inserting a Default Value
-We can insert the default value to a field by using the DEFAULT keyword during our INSERT statement. This will insert the default value into the field, or NULL if no default value is provided.
-Assuming a field is set to have a default and not null constraints, we can insert
-
-
-*/
-
--- Assuming we have 2 fields in a table, and field_1 has the default value
--- 'Magic String', and we do the following:
-INSERT INTO <table> (<field_1>, <field_2>)
-VALUES (DEFAULT, <field_2_value>);
--- This will insert into field_1 the default value 'Magic String'.
-
-
-
-
--- Example
--- DEFAULT is useful specially useful when dealing with primary keys.
--- Note: primary keys is discussed later on in this chapter.
-INSERT INTO magical_table (title, n_pages)
-VALUES (DEFAULT, 342);
-
--- Assuming a table magical_table2 with fields book_id, title, n_pages;
--- where book_id is a primary key field with auto increment:
-INSERT INTO magical_table2
-VALUES (DEFAULT, 'Gatsby The Great', 342);
-
-/*markdown
-### NULL and DEFAULT
-Even with a default value, we can still explicitly insert a NULL value into a field. If we don't want this behaviour, we can chain the NOT NULL and DEFAULT constraints.
-*/
-
-CREATE TABLE <table_name> (
-    <field> <field_type> NOT NULL DEFAULT <field_default>
+INSERT INTO cats3 (
+    name,
+    age
+)
+VALUES (
+    'Tabby',
+    DEFAULT
 );
 
--- Example
-CREATE TABLE cats4 (
-    name VARCHAR(100) NOT NULL DEFAULT 'Tabby',
-    age INT
+
+-- The above example can also be done by omitting the field with a default value.
+INSERT INTO cats3 (
+    name
+)
+VALUES (
+    'Tabby'
 );
 
--- This will not be allowed since name CAN'T be NULL.
-INSERT INTO cats4 ()
-VALUES(NULL, 3);
-
 /*markdown
-## Primary Keys
-Primary keys are unique identifiers for entries in a table. This is useful when two entries share the same value for a field. For example, in a cat's table, if there are two cats named Tabby, the primary key of each cat will allow us to distinguish between the two cats.
+## Primary Key
 
+Primary keys are unique identifiers for records in a table. This is useful when two or more entries share the same value for a field. For example, if there are two cats having the same name in our table, the primary keys will allow us to distinguish between the two.  
 
-Note that it is a convention for a primary key to be an integer data type since it is faster than strings and can be used in tandem with the AUTO_INCREMENT keyword. There are a few integer data types and they differ in the maximum number they can allow.
+Note that although primary keys can be of any data type, it is a convention for a primary key to be of integer type (specific int size might vary according to size of table), since it is faster and can be used in tandem with the AUTO_INCREMENT keyword.
 */
 
--- There are two ways to initialize a primary key field:
--- Say we want field_1 to be the primary key
+/*markdown
+### Creating a Primary Key Field
+*/
 
--- First option
+-- There are two common ways to initialize a primary key field.
+-- Say we want field_1 to be the primary key:
 CREATE TABLE <table_name> (
     <field_1> <int_type> PRIMARY KEY,
-    <field_2> <field_2_type>,
+    <field_2> <field2_type>
 );
 
--- Alternative
+
+-- Or we can declare our primary key field separately:
 CREATE TABLE <table_name> (
     <field_1> <int_type>,
-    <field_2> <field_2_type>,
-    PRIMARY KEY (field_1)
+    <field_2> <field2_type>,
+    PRIMARY KEY (<field_1>)
 );
 
 -- Notice that there is no NOT NULL constraint for the key field.
--- This is because it is reduntant since primary keys can never
--- be null in the first place.
+-- This is because primary keys can never be null in the first place,
+-- making the NOT NULL keyword redundant for primary key fields.
 
--- Example
+/*markdown
+### Creating a Primary Key Field - Example
+*/
+
 CREATE TABLE unique_cats (
     cat_id INT PRIMARY KEY,
-    name VARCHAR(100),
+    name VARCHAR(100)
 );
 
-CREATE TABLE unique_cats2 (
+
+-- The above example is identical to:
+CREATE TABLE unique_cats (
     cat_id INT,
     name VARCHAR(100),
     PRIMARY KEY (cat_id)
 );
 
 /*markdown
-### Auto Increment on Primary Keys
-To not worry about manually typing in a primary key, AUTO_INCREMENT (note the underscore) can be used. AUTO_INCREMENT automatically inserts a value for the keys incrementally.
+### Auto Increment for Primary Keys
+
+To not worry about manually typing in a primary key every time, AUTO_INCREMENT (note the underscore) can be used. AUTO_INCREMENT automatically inserts a value for the keys incrementally.  
+
+Note that even if AUTO_INCREMENT will always increment by 1 from the last assigned value it gave, even if that record is removed. e.g. We have the keys 1, 2, 3, and the records corresponding to keys 2 and 3 has been removed; when a new record is added, AUTO_INCREMENT will assign the key as 4, not 2.
 */
 
 CREATE TABLE <table_name> (
     <field_1> <int_type> PRIMARY KEY AUTO_INCREMENT,
-    <field_2> <field_2_type>,
+    <field_2> <field2_type>
 );
 
--- Example
+
+-- Alternatively:
+CREATE TABLE <table_name> (
+    <field_1> <int_type> AUTO_INCREMENT,
+    <field_2> <field2_type>,
+    PRIMARY KEY (<field_1>)
+);
+
+/*markdown
+### Auto Increment for Primary Keys - Example
+*/
+
 CREATE TABLE unique_cats3 (
     cat_id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100),
+    name VARCHAR(100)
 );
 
--- QUICK RUN CELL -- Run queries for testing here.
-DESC unique_cats3;
+-- Execute cell. Run SQL statements here (for testing or whatnot). Treat this like a command line prompt.
+SHOW TABLES;
