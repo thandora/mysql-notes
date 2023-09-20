15e13ccb1dec6362c5a59c231d077a31ccaa9fe0
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2023-09-20 15:38:26 +0800

    Create chapter 8

---
+++ mysql_08.sql
@@ -0,0 +1,4 @@
+/*markdown
+# MySQL 08 - Relationships and Joins
+Upto this point, we have been dealing with one table at a time, a self-contained basic table. In this chapter, we will learn about relationships between tables and how we can create them. We will also be learning about joining table views from our queries using the JOIN keyword.
+*/
