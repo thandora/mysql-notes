52eaa0bb65d5c339e1505fd19378417dd54eef22
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2023-10-07 17:02:57 +0800

    Add relationship and many to many section

--- mysql_08.sql
+++ mysql_08.sql
@@ -1,29 +1,42 @@
 /*markdown
 # MySQL 08 - Relationships and Joins
+
 Upto this point, we have been dealing with simple data stored in one table at a time, a self-contained basic table. In this chapter, we will learn about relationships between tables and how we can create them. We will also be learning about joining table views from our queries using the JOIN keyword.
+
 */
 
 /*markdown
 ## Keys
+
 To learn more about table relationships, we need to know about the concept of keys. Keys are identifiers used to refer to a row. A key can be classified into two: primary key and foreign key.
+
 */
 
 /*markdown
 ### Primary Key
+
 A primary key is a **unique** identifier for a row in a specific table. This primary key ensures that a row will be unique.
+
 */
 
 /*markdown
 ## Foreign Key
-A foreign key is a key in a table which refers to a primary key on another table.![keys](./img/keys.png)
+
+A foreign key is a key in a table which refers to a primary key on another table.
+
+![keys](./img/keys.png)
+
 */
 
 /*markdown
 ### Keys Example
+
 Let us create the tables and data described in the image.
+
 - `FOREIGN KEY (customer_id) REFERENCES customers(id)` establishes the relationship between the two tables.
 - When a relationship is established, inputting a foreign key that does not exist in the referenced table will not be allowed.
 - Our example is a one-to-many relationship (one customer can have many orders).
+
 */
 
 CREATE TABLE customers (
@@ -62,15 +75,36 @@ VALUES
     ('1999-04-11', 450.25, 5);
 
 /*markdown
+## Relationships
+
+Relationships are how tables relate to each other. The relationship type is determined on how a row on one table maps to another, specifically, on how many rows from, say `table B`, relates to a row in `table A`. There are three kinds of relationships:
+
+1. One to One (Uncommon)
+2. One to Many (Most Common)
+3. Many to Many (Common)
+
+A quick illustration example:
+
+![Relationship types](img/relationships.png)
+
+Note: _Assuming_ that a teacher (and headteacher) can only be employed in 1 school.
+
+*/
+
+/*markdown
 ## Joins
-Now that we have establish a relationship between tables, how do we actually make a query such that the columns between the tables are returned? That is where **JOIN** comes in. A **JOIN** clause combines rows from two or more tables, based on a related column (this is where foreign keys come in) between them.
+
+Now that we have established a relationship between tables, how do we actually make a query such that the columns between the tables are returned? That is where **JOIN** comes in. A **JOIN** clause combines rows from two or more tables, based on a related column (this is where foreign keys come in) between them.
+
 */
 
 /*markdown
 ### Join Types
+
 A join can be classified according to which rows from the two (or more) tables are returned.
 
 In MySQL, there are four native types of JOIN:
+
 1. Left
    - returns all rows from the `left`, and matched rows from the `right`.
 2. Right
@@ -80,17 +114,22 @@ In MySQL, there are four native types of JOIN:
 4. Cross
    - Returns all rows from `both tables`.
 
-Notes: 
+Notes:
+
 - `left` and `right` refers to the tables in the JOIN clause. Left table refers to the table that appears first in the clause.
 - Cross join is also known as full outer join in other SQL flavors.
 
 You can manipulate the query to achieve other types of joins outside this 4.
+
 ![mysql joins](img/mysql_joins.png)
+
 */
 
 /*markdown
 ### Cross Join
+
 Cross join is not commonly used as it just returns a (redundant) view of all the rows from both tables.
+
 */
 
 SELECT
@@ -108,7 +147,9 @@ FROM
 
 /*markdown
 ### Inner Join
+
 Inner joins are useful when we only want to records that appear in both tables.
+
 */
 
 SELECT
@@ -145,7 +186,8 @@ ORDER BY
 
 /*markdown
 ### Left Join
-Useful if we want to display the original table (left), matched with the table on the right. If a row from the left table does not match with anything on the right, the columns of the right will be **NULL**.  
+
+Useful if we want to display the original table (left), matched with the table on the right. If a row from the left table does not match with anything on the right, the columns of the right will be **NULL**.
 
 Left and right table are declared this way:
 
@@ -154,6 +196,7 @@ FROM
     <left_table>
     LEFT JOIN <right_table> ...
 ```
+
 */
 
 SELECT
@@ -161,6 +204,7 @@ SELECT
 FROM
     customers
     LEFT JOIN orders ON orders.customer_id = customers.id;
+
 -- There might be NULL values on the first column (customers.id). This is
 -- only a visual bug caused by the SQL notebook extension.
 
@@ -200,6 +244,7 @@ ORDER BY
 
 /*markdown
 ### Right Join
+
 Functionally same as left join, except this time all rows are displayed from the right table instead of the left.
 
 Left and right table are declared this way:
@@ -209,6 +254,7 @@ FROM
     <left_table>
     RIGHT JOIN <right_table> ...
 ```
+
 */
 
 SELECT
@@ -219,3 +265,10 @@ SELECT
 FROM
     customers
     RIGHT JOIN orders ON customers.id = orders.customer_id;
+
+/*markdown
+## Many to Many Relationship
+
+#TODO
+
+*/
