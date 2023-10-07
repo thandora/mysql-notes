7997479e68620a6d4e02a9c63efddf93f5fe05be
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2023-10-08 02:54:25 +0800

    Fix format

--- mysql_08.sql
+++ mysql_08.sql
@@ -1,13 +1,10 @@
 /*markdown
 # MySQL 08 - Relationships and Joins
-
 Upto this point, we have been dealing with simple data stored in one table at a time, a self-contained basic table. In this chapter, we will learn about relationships between tables and how we can create them. We will also be learning about joining table views from our queries using the JOIN keyword.
-
 */
 
 /*markdown
 ## Keys
-
 To learn more about table relationships, we need to know about the concept of keys. Keys are identifiers used to refer to a row. A key can be classified into two: primary key and foreign key.
 
 */
@@ -21,7 +18,6 @@ A primary key is a **unique** identifier for a row in a specific table. This pri
 
 /*markdown
 ## Foreign Key
-
 A foreign key is a key in a table which refers to a primary key on another table.
 
 ![keys](./img/keys.png)
@@ -30,7 +26,6 @@ A foreign key is a key in a table which refers to a primary key on another table
 
 /*markdown
 ### Keys Example
-
 Let us create the tables and data described in the image.
 
 - `FOREIGN KEY (customer_id) REFERENCES customers(id)` establishes the relationship between the two tables.
@@ -76,7 +71,6 @@ VALUES
 
 /*markdown
 ## Relationships
-
 Relationships are how tables relate to each other. The relationship type is determined on how a row on one table maps to another, specifically, on how many rows from, say `table B`, relates to a row in `table A`. There are three kinds of relationships:
 
 1. One to One (Uncommon)
@@ -93,14 +87,12 @@ Note: _Assuming_ that a teacher (and headteacher) can only be employed in 1 scho
 
 /*markdown
 ## Joins
-
 Now that we have established a relationship between tables, how do we actually make a query such that the columns between the tables are returned? That is where **JOIN** comes in. A **JOIN** clause combines rows from two or more tables, based on a related column (this is where foreign keys come in) between them.
 
 */
 
 /*markdown
 ### Join Types
-
 A join can be classified according to which rows from the two (or more) tables are returned.
 
 In MySQL, there are four native types of JOIN:
@@ -129,7 +121,6 @@ You can manipulate the query to achieve other types of joins outside this 4.
 ### Cross Join
 
 Cross join is not commonly used as it just returns a (redundant) view of all the rows from both tables.
-
 */
 
 SELECT
@@ -147,16 +138,14 @@ FROM
 
 /*markdown
 ### Inner Join
-
 Inner joins are useful when we only want to records that appear in both tables.
-
 */
 
 SELECT
     *
 FROM
     customers
-    JOIN orders ON orders.customer_id = customers.id;
+    JOIN orders ON customers.id = orders.customer_id;
 
 -- If a column is ambiguous, like the 'id' column we have which exists in both table,
 -- we should specify the table where it came from: <table>.<column>
@@ -168,7 +157,7 @@ SELECT
     amount
 FROM
     customers
-    JOIN orders ON orders.customer_id = customers.id;
+    JOIN orders ON customers.id = orders.customer_id;
 
 -- Joins is more powerful when paired with groupings and aggregate functions
 SELECT
@@ -177,7 +166,7 @@ SELECT
     SUM(amount) AS total
 FROM
     customers
-    JOIN orders ON orders.customer_id = customers.id
+    JOIN orders ON customers.id = orders.customer_id
 GROUP BY
     first_name,
     last_name
@@ -186,7 +175,6 @@ ORDER BY
 
 /*markdown
 ### Left Join
-
 Useful if we want to display the original table (left), matched with the table on the right. If a row from the left table does not match with anything on the right, the columns of the right will be **NULL**.
 
 Left and right table are declared this way:
@@ -196,14 +184,13 @@ FROM
     <left_table>
     LEFT JOIN <right_table> ...
 ```
-
 */
 
 SELECT
     *
 FROM
     customers
-    LEFT JOIN orders ON orders.customer_id = customers.id;
+    LEFT JOIN orders ON customers.id = orders.customer_id;
 
 -- There might be NULL values on the first column (customers.id). This is
 -- only a visual bug caused by the SQL notebook extension.
@@ -216,7 +203,7 @@ SELECT
     amount
 FROM
     customers
-    LEFT JOIN orders ON orders.customer_id = customers.id;
+    LEFT JOIN orders ON customers.id = orders.customer_id;
 
 -- Example where the 'orders' table is the left table.
 SELECT
@@ -244,7 +231,6 @@ ORDER BY
 
 /*markdown
 ### Right Join
-
 Functionally same as left join, except this time all rows are displayed from the right table instead of the left.
 
 Left and right table are declared this way:
@@ -254,7 +240,6 @@ FROM
     <left_table>
     RIGHT JOIN <right_table> ...
 ```
-
 */
 
 SELECT
@@ -268,7 +253,70 @@ FROM
 
 /*markdown
 ## Many to Many Relationship
-
-#TODO
-
+Lorem impsum
 */
+
+/*markdown
+### Data
+*/
+
+-- Tables
+CREATE TABLE reviewers (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE series (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(100),
+    released_year YEAR,
+    genre VARCHAR(100)
+);
+
+CREATE TABLE reviews (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    rating DECIMAL(2, 1),
+    series_id INT,
+    reviewer_id INT,
+    FOREIGN KEY (series_id) REFERENCES series (id),
+    FOREIGN KEY (reviewer_id) REFERENCES reviewers (id)
+);
+
+-- Data
+INSERT INTO series (title, released_year, genre) VALUES
+    ('Archer', 2009, 'Animation'),
+    ('Arrested Development', 2003, 'Comedy'),
+    ("Bob's Burgers", 2011, 'Animation'),
+    ('Bojack Horseman', 2014, 'Animation'),
+    ("Breaking Bad", 2008, 'Drama'),
+    ('Curb Your Enthusiasm', 2000, 'Comedy'),
+    ("Fargo", 2014, 'Drama'),
+    ('Freaks and Geeks', 1999, 'Comedy'),
+    ('General Hospital', 1963, 'Drama'),
+    ('Halt and Catch Fire', 2014, 'Drama'),
+    ('Malcolm In The Middle', 2000, 'Comedy'),
+    ('Pushing Daisies', 2007, 'Comedy'),
+    ('Seinfeld', 1989, 'Comedy'),
+    ('Stranger Things', 2016, 'Drama');
+ 
+INSERT INTO reviewers (first_name, last_name) VALUES
+    ('Thomas', 'Stoneman'),
+    ('Wyatt', 'Skaggs'),
+    ('Kimbra', 'Masters'),
+    ('Domingo', 'Cortes'),
+    ('Colt', 'Steele'),
+    ('Pinkie', 'Petit'),
+    ('Marlon', 'Crafford');
+    
+INSERT INTO reviews(series_id, reviewer_id, rating) VALUES
+    (1,1,8.0),(1,2,7.5),(1,3,8.5),(1,4,7.7),(1,5,8.9),
+    (2,1,8.1),(2,4,6.0),(2,3,8.0),(2,6,8.4),(2,5,9.9),
+    (3,1,7.0),(3,6,7.5),(3,4,8.0),(3,3,7.1),(3,5,8.0),
+    (4,1,7.5),(4,3,7.8),(4,4,8.3),(4,2,7.6),(4,5,8.5),
+    (5,1,9.5),(5,3,9.0),(5,4,9.1),(5,2,9.3),(5,5,9.9),
+    (6,2,6.5),(6,3,7.8),(6,4,8.8),(6,2,8.4),(6,5,9.1),
+    (7,2,9.1),(7,5,9.7),(8,4,8.5),(8,2,7.8),(8,6,8.8),
+    (8,5,9.3),(9,2,5.5),(9,3,6.8),(9,4,5.8),(9,6,4.3),
+    (9,5,4.5),(10,5,9.9),(13,3,8.0),(13,4,7.2),(14,2,8.5),
+    (14,3,8.9),(14,4,8.9);
