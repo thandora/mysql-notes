f041d442efc84d5e0d43618b97a147d4bc1ef6fe
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2023-10-15 22:04:19 +0800

    Finish first complete version
    Done by finishing the "many to many relationships" section.

--- mysql_08.sql
+++ mysql_08.sql
@@ -6,32 +6,25 @@ Upto this point, we have been dealing with simple data stored in one table at a 
 /*markdown
 ## Keys
 To learn more about table relationships, we need to know about the concept of keys. Keys are identifiers used to refer to a row. A key can be classified into two: primary key and foreign key.
-
 */
 
 /*markdown
 ### Primary Key
-
 A primary key is a **unique** identifier for a row in a specific table. This primary key ensures that a row will be unique.
-
 */
 
 /*markdown
 ## Foreign Key
 A foreign key is a key in a table which refers to a primary key on another table.
-
 ![keys](./img/keys.png)
-
 */
 
 /*markdown
 ### Keys Example
 Let us create the tables and data described in the image.
-
 - `FOREIGN KEY (customer_id) REFERENCES customers(id)` establishes the relationship between the two tables.
 - When a relationship is established, inputting a foreign key that does not exist in the referenced table will not be allowed.
 - Our example is a one-to-many relationship (one customer can have many orders).
-
 */
 
 CREATE TABLE customers (
@@ -72,31 +65,26 @@ VALUES
 /*markdown
 ## Relationships
 Relationships are how tables relate to each other. The relationship type is determined on how a row on one table maps to another, specifically, on how many rows from, say `table B`, relates to a row in `table A`. There are three kinds of relationships:
-
 1. One to One (Uncommon)
 2. One to Many (Most Common)
 3. Many to Many (Common)
 
-A quick illustration example:
-
-![Relationship types](img/relationships.png)
-
+A quick example:  
+![Relationship types](img/relationships.png)  
 Note: _Assuming_ that a teacher (and headteacher) can only be employed in 1 school.
-
 */
 
 /*markdown
 ## Joins
 Now that we have established a relationship between tables, how do we actually make a query such that the columns between the tables are returned? That is where **JOIN** comes in. A **JOIN** clause combines rows from two or more tables, based on a related column (this is where foreign keys come in) between them.
-
 */
 
 /*markdown
 ### Join Types
-A join can be classified according to which rows from the two (or more) tables are returned.
+![mysql joins](img/mysql_joins.png)
 
+A join can be classified according to which rows from the two (or more) tables are returned.  
 In MySQL, there are four native types of JOIN:
-
 1. Left
    - returns all rows from the `left`, and matched rows from the `right`.
 2. Right
@@ -107,19 +95,13 @@ In MySQL, there are four native types of JOIN:
    - Returns all rows from `both tables`.
 
 Notes:
-
 - `left` and `right` refers to the tables in the JOIN clause. Left table refers to the table that appears first in the clause.
 - Cross join is also known as full outer join in other SQL flavors.
-
-You can manipulate the query to achieve other types of joins outside this 4.
-
-![mysql joins](img/mysql_joins.png)
-
+- You can manipulate the query to achieve other types of joins outside this 4.
 */
 
 /*markdown
 ### Cross Join
-
 Cross join is not commonly used as it just returns a (redundant) view of all the rows from both tables.
 */
 
@@ -175,10 +157,8 @@ ORDER BY
 
 /*markdown
 ### Left Join
-Useful if we want to display the original table (left), matched with the table on the right. If a row from the left table does not match with anything on the right, the columns of the right will be **NULL**.
-
-Left and right table are declared this way:
-
+Useful if we want to display the original table (left), matched with the table on the right. If a row from the left table does not match with anything on the right, the columns of the right will be **NULL**.  
+Left and right tables are declared this way:  
 ```
 FROM
     <left_table>
@@ -191,7 +171,6 @@ SELECT
 FROM
     customers
     LEFT JOIN orders ON customers.id = orders.customer_id;
-
 -- There might be NULL values on the first column (customers.id). This is
 -- only a visual bug caused by the SQL notebook extension.
 
@@ -231,10 +210,8 @@ ORDER BY
 
 /*markdown
 ### Right Join
-Functionally same as left join, except this time all rows are displayed from the right table instead of the left.
-
-Left and right table are declared this way:
-
+Functionally same as left join, except this time all rows are displayed from the right table instead of the left.  
+Left and right table are declared this way:  
 ```
 FROM
     <left_table>
@@ -253,11 +230,12 @@ FROM
 
 /*markdown
 ## Many to Many Relationship
-Lorem impsum
+Let's look at some operations involving many to many relationships.
 */
 
 /*markdown
 ### Data
+We will be dealing with reviews on some TV series. Our tables will be data on **reviewers**, **reviews**, and **tv series**.
 */
 
 -- Tables
@@ -320,3 +298,91 @@ INSERT INTO reviews(series_id, reviewer_id, rating) VALUES
     (8,5,9.3),(9,2,5.5),(9,3,6.8),(9,4,5.8),(9,6,4.3),
     (9,5,4.5),(10,5,9.9),(13,3,8.0),(13,4,7.2),(14,2,8.5),
     (14,3,8.9),(14,4,8.9);
+
+/*markdown
+### Exercises
+*/
+
+-- Show titles and respective ratings
+SELECT
+    title,
+    rating
+FROM
+    reviews
+    JOIN series ON reviews.series_id = series.id
+LIMIT 8;
+
+-- Show titles and their average rating, sorted from lowest.
+SELECT
+	title,
+	AVG(rating) as avg_rating
+FROM
+	series
+	JOIN reviews ON series.id = reviews.series_id
+GROUP BY
+	title
+ORDER BY
+	avg_rating
+LIMIT 8;
+
+-- Reviewers' names and their ratings 
+-- (rating doesn't need to be specified for what for now)
+SELECT
+	first_name,
+    last_name,
+    rating
+FROM
+	reviewers
+    JOIN reviews ON reviewers.id = reviews.reviewer_id
+LIMIT 8;
+
+-- Title of unreviewed series
+SELECT
+	title as unreviewed_series
+FROM
+	series
+    LEFT JOIN reviews ON series.id = reviews.series_id
+WHERE 
+	rating IS NULL
+LIMIT 8;
+
+-- Average rating of genre
+SELECT
+	genre,
+    AVG(rating)
+FROM
+	series
+    JOIN reviews ON series.id = reviews.series_id
+GROUP BY
+	genre
+LIMIT 8;
+
+-- Name of reviewer, their rating stats, and status.
+SELECT
+	first_name,
+    last_name,
+    COUNT(rating),
+    IFNULL(MIN(rating), 0) as 'min',
+    IFNULL(MAX(rating), 0) as 'max',
+    IFNULL(ROUND(AVG(rating), 2), 0) as 'avg',
+    IF(COUNT(rating) != 0, 
+    'ACTIVE', 
+    'INACTIVE'
+    ) as 'status'
+FROM
+	reviewers
+    LEFT JOIN reviews ON reviewers.id = reviews.reviewer_id
+GROUP BY
+	last_name,
+    first_name
+LIMIT 8;
+
+-- Reviews on series and the reviewer
+SELECT
+	title,
+    rating,
+    CONCAT(first_name, ' ', last_name) as 'reviewer'
+FROM
+	reviews
+    LEFT JOIN series ON reviews.series_id = series.id
+    LEFT JOIN reviewers ON reviews.reviewer_id = reviewers.id;
