336e216a23cee2a7d7d6eebb1d98b3f80eff808e
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2023-10-21 23:55:57 +0800

    Create draft 1
    Created draft for views and having clause.

--- mysql_09.sql
+++ mysql_09.sql
@@ -15,3 +15,39 @@ SHOW FULL TABLES;
 
 -- Delete a view. Keywords on [BRACKETS] is optional.
 DROP VIEW [IF EXISTS] <view_name>;
+
+-- Some views are updatable
+-- https://dev.mysql.com/doc/refman/8.0/en/view-updatability.html
+
+-- Create view
+CREATE VIEW ordered_series AS
+SELECT * FROM series ORDER BY released_year;
+ 
+-- Replace a view
+CREATE OR REPLACE VIEW ordered_series AS
+SELECT * FROM series ORDER BY released_year DESC;
+ 
+-- Replace using different keyword
+ALTER VIEW ordered_series AS
+SELECT * FROM series ORDER BY released_year;
+
+-- Having
+-- Filter results after GROUPING (based on *conditions on aggregated data*)
+SELECT 
+    title, 
+    AVG(rating),
+    COUNT(rating) AS review_count
+FROM full_reviews 
+GROUP BY 
+    title HAVING COUNT(rating) > 1;
+
+-- WHERE
+-- Filter results before GROUPING
+SELECT 
+    title, 
+    AVG(rating),
+    COUNT(rating) AS review_count
+FROM full_reviews 
+WHERE released_year = 2016
+GROUP BY 
+    title ;
